a0078951d93942441fb967126ce9c204e24fe441
Kevin Lin <kevin.lin.p@gmail.com>
2019-12-02 19:34:15 -0500

    first 2 days

--- 2.js
+++ 2.js
@@ -1,23 +1,45 @@
-const moduleWeights = [68936, 53526, 62556, 115539, 119659, 77887, 101443, 71392, 130327, 56769, 55083, 101448, 63985, 60433, 80302, 101264, 134416, 112047, 143310, 73842, 124020, 50346, 124192, 119547, 59351, 122161, 103742, 107648, 132879, 65047, 70234, 54569, 72785, 120259, 134533, 61778, 89183, 144270, 68600, 134849, 120221, 126887, 128483, 101293, 78066, 141762, 101929, 119173, 148580, 142710, 142029, 61303, 133204, 120872, 141111, 124900, 73600, 73552, 138183, 147019, 63157, 127712, 83610, 59098, 101675, 57951, 146696, 135604, 75158, 140629, 106125, 142451, 59468, 69078, 115676, 69763, 129999, 97987, 64654, 104168, 67894, 92675, 125475, 110450, 52738, 87569, 91939, 117714, 121018, 140534, 97876, 146651, 105741, 140417, 74771, 141727, 94957, 145126, 61429, 143890];
+// const memory = [1,0,0,0,99]
+// const memory = [1,9,10,3,2,3,11,0,99,30,40,50]
+//const memory = [1,1,1,4,99,5,6,0,99]
 
-const fuelRequirement = function (m) {
-  const fuel = Math.floor(m / 3) - 2;
-  
-  if (fuel <= 0) {
-    return 0;
-  } else {
-    return fuel + fuelRequirement(fuel);
+function program(noun, verb) {
+  const memory = [1,noun,verb,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,9,23,1,23,9,27,1,10,27,31,1,13,31,35,1,35,10,39,2,39,9,43,1,43,13,47,1,5,47,51,1,6,51,55,1,13,55,59,1,59,6,63,1,63,10,67,2,67,6,71,1,71,5,75,2,75,10,79,1,79,6,83,1,83,5,87,1,87,6,91,1,91,13,95,1,95,6,99,2,99,10,103,1,103,6,107,2,6,107,111,1,13,111,115,2,115,10,119,1,119,5,123,2,10,123,127,2,127,9,131,1,5,131,135,2,10,135,139,2,139,9,143,1,143,2,147,1,5,147,0,99,2,0,14,0];
+
+  let pointer = 0;
+  let terminated = false;
+
+  while (!terminated) {
+    const operandAIndex = memory[pointer + 1];
+    const operandBIndex = memory[pointer + 2];
+    const writeIndex = memory[pointer + 3];
+
+    const operandA = memory[operandAIndex];
+    const operandB = memory[operandBIndex];
+
+    switch (memory[pointer]) {
+      case 1:
+        memory[writeIndex] = operandA + operandB;
+        break;
+      case 2:
+        memory[writeIndex] = operandA * operandB;
+        break;
+      case 99:
+        return memory[0];
+        break;
+      default:
+        new Error();
+    }
+
+    pointer += 4;
   }
-};
+}
 
-const fuelRequirements = moduleWeights.map(m => {
-  return fuelRequirement(m);
-});
+for (i = 0; i < 100; i++) {
+  for (j = 0; j < 100; j++) {
+    const result = program(i, j);
+    if (result == 19690720) {
+      console.log(`noun:${i} verb:${j} ${100 * i + j}`);
+    }
+  }
+}
 
-console.log(fuelRequirements);
-
-const total = fuelRequirements.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue;
-}, 0);
-
-console.log(total);
