c3be83733669349975973c3618a8903c5ddb7972
Kevin Lin <kevin.lin.p@gmail.com>
2019-12-08 18:12:59 -0500

    day 7 pt 2 refactoring

--- 7.py
+++ 7.py
@@ -9,6 +9,48 @@ class IntcodeComputer:
         # self.memory = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
         self.memory = [3,8,1001,8,10,8,105,1,0,0,21,30,55,76,97,114,195,276,357,438,99999,3,9,102,3,9,9,4,9,99,3,9,1002,9,3,9,1001,9,5,9,1002,9,2,9,1001,9,2,9,102,2,9,9,4,9,99,3,9,1002,9,5,9,1001,9,2,9,102,5,9,9,1001,9,4,9,4,9,99,3,9,1001,9,4,9,102,5,9,9,101,4,9,9,1002,9,4,9,4,9,99,3,9,101,2,9,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99]
 
+    def parse_parameters(self, opcode):
+        num_parameters = {
+            1: 3,
+            2: 3,
+            3: 1,
+            4: 1,
+            5: 2,
+            6: 2,
+            7: 3,
+            8: 3,
+        }[opcode]
+
+        num_read_parameters = {
+            1: 2,
+            2: 2,
+            3: 0,
+            4: 1,
+            5: 2,
+            6: 2,
+            7: 2,
+            8: 2,
+        }[opcode]
+
+        parameters = map(lambda i: self.memory[self.pointer + 1 + i], range(0, num_parameters))
+        parameters = list(parameters)
+        parameter_modes = self.memory[self.pointer] // 100
+
+        for i in range(0, num_read_parameters):
+            parameter_mode = parameter_modes % 10
+            if parameter_mode == 0:
+                parameters[i] = self.memory[parameters[i]]
+            elif parameter_mode == 1:
+                parameters[i]
+            else:
+                print(parameters)
+                print(parameter_mode)
+                raise Exception()
+
+            parameter_modes = parameter_modes // 10
+
+        return parameters
+
     def run(self, runInput = []):
         if self.status == 'stopped':
             raise Exception()
@@ -25,44 +67,7 @@ class IntcodeComputer:
                 self.status = 'stopped'
                 break
 
-            num_parameters = {
-                1: 3,
-                2: 3,
-                3: 1,
-                4: 1,
-                5: 2,
-                6: 2,
-                7: 3,
-                8: 3,
-            }[opcode]
-
-            num_read_parameters = {
-                1: 2,
-                2: 2,
-                3: 0,
-                4: 1,
-                5: 2,
-                6: 2,
-                7: 2,
-                8: 2,
-            }[opcode]
-
-            parameters = map(lambda i: self.memory[self.pointer + 1 + i], range(0, num_parameters))
-            parameters = list(parameters)
-            parameter_modes = self.memory[self.pointer] // 100
-
-            for i in range(0, num_read_parameters):
-                parameter_mode = parameter_modes % 10
-                if parameter_mode == 0:
-                    parameters[i] = self.memory[parameters[i]]
-                elif parameter_mode == 1:
-                    parameters[i]
-                else:
-                    print(parameters)
-                    print(parameter_mode)
-                    raise Exception()
-
-                parameter_modes = parameter_modes // 10
+            parameters = self.parse_parameters(opcode)
 
             if opcode == 1:
                 self.memory[parameters[2]] = parameters[0] + parameters[1]
@@ -88,7 +93,7 @@ class IntcodeComputer:
                 raise Exception()
 
             if should_increment_pointer:
-                self.pointer += (num_parameters + 1)
+                self.pointer += (len(parameters) + 1)
 
         # print(self.memory)
         return self.status
